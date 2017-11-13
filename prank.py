ca12a7d8073a3b96011f93c8cd0d0349c63f0cb5
James B <Toxic.Raptor24@gmail.com>
2017-11-13 16:43:05 +0000

    Update prank.py

--- prank.py
+++ prank.py
@@ -4,9 +4,9 @@ import sys
 api = clockwork.API('')
 
 message = clockwork.SMS(
-    to = sys.argv[1],
-    from_name = sys.argv[2],
-    message = sys.argv[3])
+    to = raw_input('Going too:'),
+    from_name = raw_input('Coming from:'),
+    message = raw_input('What do we want to say?:'))
 
 response = api.send(message)
 
